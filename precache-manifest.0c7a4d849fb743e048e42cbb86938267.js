dcfb27c971be73875005c56b6f2eb75f32cb1fb8
Chukharkin <chuharkin2010@gmail.com>
2019-11-27 21:41:40 +0200

    Updates

---
+++ precache-manifest.0c7a4d849fb743e048e42cbb86938267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0952f5d8d9bbab1c0b9436ad5215cc0",
+    "url": "/HW1_Chuharkin_Build/index.html"
+  },
+  {
+    "revision": "18035289c82429347f47",
+    "url": "/HW1_Chuharkin_Build/static/css/main.51b39e3b.chunk.css"
+  },
+  {
+    "revision": "2e3837978f753a4d3537",
+    "url": "/HW1_Chuharkin_Build/static/js/2.511d3a3a.chunk.js"
+  },
+  {
+    "revision": "18035289c82429347f47",
+    "url": "/HW1_Chuharkin_Build/static/js/main.2c6dddd9.chunk.js"
+  },
+  {
+    "revision": "1415ec8dbd93ff432c2f",
+    "url": "/HW1_Chuharkin_Build/static/js/runtime-main.35a960eb.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/HW1_Chuharkin_Build/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "e45d9d1f63b5b15b9ebf933e7d9088d3",
+    "url": "/HW1_Chuharkin_Build/static/media/photo.e45d9d1f.png"
+  }
+]);
